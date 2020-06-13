d99afd46b8237017cc05380e39113a32f26d8900
NateOwens <nathanieltylerowens@gmail.com>
2020-06-13 10:48:11 -0500

    added for merge

--- src/javascripts/components/clearAllMessages/clearAllMessages.js
+++ src/javascripts/components/clearAllMessages/clearAllMessages.js
@@ -1,7 +1,17 @@
 import getMessages from '../../helpers/data/messageData';
+import utils from '../../helpers/utils';
+
+const clearAll = () => {
+  const domString = `
+  <button type="button" id="clearMessages" class="btn btn-warning">Clear All</button>`;
+  utils.printToDom('#clearAll', domString);
+};
 
 const clearMessages = () => {
   getMessages.getMessages.length = 0;
+  clearAll();
 };
 
-export default { clearMessages };
+$('body').on('click', '#clearMessages', clearMessages);
+
+export default { clearAll };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,12 +4,14 @@ import 'bootstrap';
 import userRadios from './components/userRadios';
 import listener from './components/newMessage';
 import largerText from './components/largerText/largerText';
+import clearAll from './components/clearAllMessages/clearAllMessages';
 
 const init = () => {
   listener.addMessageListener();
   userRadios.printRadioButtons();
   displayMessage.displayMessage();
   largerText.largerTextListener();
+  clearAll.clearAll();
 };
 
 init();
