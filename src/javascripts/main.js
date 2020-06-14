67d54cec276612297543092d086d4c5063766e1c
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-14 00:37:06 -0500

    in progress

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,12 +4,14 @@ import 'bootstrap';
 import userRadios from './components/userRadios';
 import listener from './components/newMessage';
 import largerText from './components/largerText/largerText';
+import searchGiphy from './components/searchGiphy/searchGiphy';
 
 const init = () => {
   listener.addMessageListener();
   userRadios.printRadioButtons();
   displayMessage.displayMessage();
   largerText.largerTextListener();
+  searchGiphy.formListener();
 };
 
 init();
