40db6caf2d4691d7724fc6b6dc6dce0ac037d0fd
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-15 16:57:57 -0500

    refectored index and styled page

--- src/javascripts/components/newMessage/newMessage.js
+++ src/javascripts/components/newMessage/newMessage.js
@@ -6,32 +6,33 @@ import './newMessage.scss';
 const printMessageForm = () => {
   let domString = '';
   domString += `
-    <div id="gifs" class="d-flex justify-content-center flex-wrap"></div>
     <div class="collapse" id="gif-selector">
+      <div id="gifs" class="d-flex justify-content-center flex-wrap"></div>
       <div class="card card-body">
-        <form class="form-inline mx-auto" id="search-giphy">
-          <div class="form-group mx-sm-3 mb-2">
+        <form class="form" id="search-giphy">
+          <div class="form-group">
             <input type="text" class="form-control" id="gifPhrase" placeholder="Search for a Gif">
           </div>
           <button type="submit" class="btn btn-primary mb-2">Search Gifs</button>
         </form>
       </div>
     </div>
-    <form class="form-inline mx-auto" id="new-message-form">
-    <div class="form-row align-items-center">
-      <div class="form-group">
-        <textarea class="form-control m-3" id="new-message" name="message" placeholder="Enter your message here..."></textarea>
-        <button type="submit" class="btn btn-primary m-2">Submit</button>
+
+    <form id="new-message-form">
+      <div class="form-row">
+        <textarea class="form-control col-md-6 m-3" id="new-message" name="message" placeholder="Enter your message here..."></textarea>
+        <button type="submit" class="btn btn-primary form-control col-md-1 m-3">Submit</button>
+        <button class="btn btn-primary form-control col-md-1 m-3" type="button" data-toggle="collapse" data-target="#gif-selector" aria-expanded="false" aria-controls="collapseExample">
+          Giphy
+        </button>
       </div>
-      <button class="btn btn-primary m-2" type="button" data-toggle="collapse" data-target="#gif-selector" aria-expanded="false" aria-controls="collapseExample">
-        Giphy
-      </button>
-    </div>
+      <div class="form-group m-3 text-light">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="largerText">
+          <label class="form-check-label" for="largerText">Make Text Larger</label>
+        </div>
+      </div>
     </form>
-    <div class="form-check m-3">
-      <input type="checkbox" class="form-check-input" id="largerText">
-      <label class="form-check-label" for="largerText">Make Text Larger</label>
-    </div>
   `;
   utils.printToDom('message-entry', domString);
 };
@@ -50,6 +51,7 @@ const addMessageToArray = (e) => {
   displayMessage.displayMessage();
   $('#gif-selector').collapse('hide');
   document.querySelector('#gifs').innerHTML = '';
+  $('#messageCard').animate({ scrollTop: $('#messageCard')[0].scrollHeight }, 1000);
 };
 
 const addMessageListener = () => {
