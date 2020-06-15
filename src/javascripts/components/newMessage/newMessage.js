19655b9f3a306016fb2628a759a1495cc672bc63
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-06-15 03:51:08 -0500

    in progress

--- src/javascripts/components/newMessage/newMessage.js
+++ src/javascripts/components/newMessage/newMessage.js
@@ -1,5 +1,40 @@
 import messageData from '../../helpers/data/messageData';
 import displayMessage from '../displayMessage/displayMessage';
+import utils from '../../helpers/utils';
+import './newMessage.scss';
+
+const printMessageForm = () => {
+  let domString = '';
+  domString += `
+    <div id="gifs" class="d-flex justify-content-center flex-wrap"></div>
+    <div class="collapse" id="gif-selector">
+      <div class="card card-body">
+        <form class="form-inline mx-auto" id="search-giphy">
+          <div class="form-group mx-sm-3 mb-2">
+            <input type="text" class="form-control" id="gifPhrase" placeholder="Search for a Gif">
+          </div>
+          <button type="submit" class="btn btn-primary mb-2">Search Gifs</button>
+        </form>
+      </div>
+    </div>
+    <form class="form-inline mx-auto" id="new-message-form">
+    <div class="form-row align-items-center">
+      <div class="form-group">
+        <textarea class="form-control m-3" id="new-message" name="message" placeholder="Enter your message here..."></textarea>
+        <button type="submit" class="btn btn-primary m-2">Submit</button>
+      </div>
+      <button class="btn btn-primary m-2" type="button" data-toggle="collapse" data-target="#gif-selector" aria-expanded="false" aria-controls="collapseExample">
+        Giphy
+      </button>
+    </div>
+    </form>
+    <div class="form-check m-3">
+      <input type="checkbox" class="form-check-input" id="largerText">
+      <label class="form-check-label" for="largerText">Make Text Larger</label>
+    </div>
+  `;
+  utils.printToDom('message-entry', domString);
+};
 
 const addMessageToArray = (e) => {
   e.preventDefault();
@@ -21,4 +56,4 @@ const addMessageListener = () => {
   document.querySelector('#new-message-form').addEventListener('submit', addMessageToArray);
 };
 
-export default { addMessageListener };
+export default { addMessageListener, printMessageForm };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,12 +2,13 @@ import '../styles/main.scss';
 import displayMessage from './components/displayMessage/displayMessage';
 import 'bootstrap';
 import userRadios from './components/userRadios/userRadios';
-import listener from './components/newMessage/newMessage';
+import newMessage from './components/newMessage/newMessage';
 import largerText from './components/largerText/largerText';
 import searchGiphy from './components/searchGiphy/searchGiphy';
 
 const init = () => {
-  listener.addMessageListener();
+  newMessage.printMessageForm();
+  newMessage.addMessageListener();
   userRadios.printRadioButtons();
   displayMessage.displayMessage();
   largerText.largerTextListener();
